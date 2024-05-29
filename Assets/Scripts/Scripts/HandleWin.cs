29b52ecd2883d189b3c726a4b1622a0712d0821d
KaryVelle <105183305+KaryVelle@users.noreply.github.com>
2024-05-29 02:57:44 -0600

    adding podium

--- Assets/Scripts/Scripts/HandleWin.cs
+++ Assets/Scripts/Scripts/HandleWin.cs
@@ -1,24 +1,80 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Mirror;
+using Mirror.Examples.Benchmark;
 using UnityEngine;
 
-public class HandleWin : MonoBehaviour
+public class HandleWin : NetworkBehaviour
 {
-
     [SerializeField] private List<GameObject> winners;
     [SerializeField] private Transform firstPlaceTransform;
     [SerializeField] private Transform secondPlaceTransform;
     [SerializeField] private Transform thirdPlaceTransform;
-        
-    public void GetWinners( GameObject winner)
+    
+
+    [SerializeField] private GameObject vcPodium;
+    [SerializeField] private GameObject _virtualCameraGameObject;
+    [SerializeField] private CinemachineVirtualCamera vc;
+
+    public override void OnStartAuthority()
+    {
+        vcPodium = GameObject.FindGameObjectWithTag("PodiumCam");
+        _virtualCameraGameObject = GameObject.FindGameObjectWithTag("PlayerFollowCamera");
+        vc = _virtualCameraGameObject.GetComponent<CinemachineVirtualCamera>();
+        firstPlaceTransform = GameObject.FindGameObjectWithTag("FirstPlace").transform;
+        secondPlaceTransform = GameObject.FindGameObjectWithTag("SecondPlace").transform;
+        thirdPlaceTransform = GameObject.FindGameObjectWithTag("ThirdPlace").transform;
+    }
+
+    public override void OnStartServer()
+    {
+        vcPodium.SetActive(false);
+    }
+
+    public void GetWinners(GameObject winner)
     {
         winners.Add(winner);
-        var characterController = winner.GetComponent<CharacterController>();
-        characterController.enabled = false;
+        Debug.Log(winners);
 
-        var firstPlace = winners[winners.Count];
-        var secondPlace = winners[winners.Count - 1];
-        var thirdPlace = winners[winners.Count - 2];
+        if (winners.Count == 1)
+        {
+            var firstPlace = winners[0];
+            var characterController1 = firstPlace.GetComponent<CharacterController>();
+            characterController1.enabled = false;
+            characterController1.gameObject.transform.position = firstPlaceTransform.position;
+            characterController1.enabled = true;
+            var randomPlayer = GameObject.FindGameObjectWithTag("Player");
+            vc.Follow = randomPlayer.transform;
+            vc.LookAt = randomPlayer.transform;
+
+
+        }
+
+        if (winners.Count == 2)
+        {
+            var secondPlace = winners[1];
+            var characterController2 = secondPlace.GetComponent<CharacterController>();
+            characterController2.enabled = false;
+            characterController2.gameObject.transform.position = secondPlaceTransform.position;
+            characterController2.enabled = true;
+            vcPodium.SetActive(true);
+           
+           
+        }
+
+       // if (winners.Count == 3)
+        //{
+         //   var thirdPlace = winners[2];
+         //   var characterController3 = thirdPlace.GetComponent<CharacterController>();
+         //   characterController3.enabled = false;
+        //    characterController3.gameObject.transform.position = thirdPlaceTransform.position;
+        //   characterController3.enabled = true;
+           
+           
+       // }
+
     }
 }
+
