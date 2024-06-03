c5d587fea5d8432c8e7edc0e8289448ed698b8c1
KaryVelle <105183305+KaryVelle@users.noreply.github.com>
2024-06-03 04:50:44 -0600

    Go audio

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    private void Update()
+    {
+       
+    }
+}
